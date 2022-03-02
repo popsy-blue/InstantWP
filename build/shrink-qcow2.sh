ccb5f42090128e5bd1527af2d9f23e96f02fa7e6
popsy-blue <---->
2022-03-02 22:38:44 +0100

    Make script independent of (local) directories.

--- build/shrink-qcow2.sh
+++ build/shrink-qcow2.sh
@@ -5,10 +5,10 @@ echo    QEMU Shrink VM Script
 echo ----------------------------
 cd "${0%/*}"
 
-read VM_VERSION < /Users/seamus/GitHub/InstantWP/build/VM_VERSION.txt
-VM_ROOT=/Users/seamus/GitHub/InstantWP/core/vm/
+read VM_VERSION < ./VM_VERSION.txt
+VM_ROOT=../core/vm/
 VM_FILE="$VM_VERSION".qcow2
 VM_IN=$VM_ROOT/$VM_FILE
-VM_OUT=$VM_ROOT/small.qcow2 
+VM_OUT=$VM_ROOT/"$VM_VERSION".small.qcow2
 
-/Users/seamus/GitHub/InstantWP/core/platform/osx/qemu/bin/qemu-img convert -O qcow2 -c $VM_IN $VM_OUT
+qemu-img convert -O qcow2 -c $VM_IN $VM_OUT
